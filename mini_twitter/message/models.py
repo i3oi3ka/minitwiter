94a8596b3256842c23f648f26444381b6b097b07
i3oi3ka <i3oi3ka@ukr.net>
2023-10-24 22:53:32 +0300

    add env.

--- mini_twitter/message/models.py
+++ mini_twitter/message/models.py
@@ -13,3 +13,7 @@ class Message(models.Model):
 
     def get_absolute_url(self):
         return reverse('message_detail', kwargs={'pk': self.pk})
+
+
+    def __str__(self):
+        return f'{self.title} {self.text}'
